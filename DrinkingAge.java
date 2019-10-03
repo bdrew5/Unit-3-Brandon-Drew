5beb67aa9ac29469d270c6096fa336535d0d3345
Brandon Drew <drewb@wlhs.wlwv.k12.or.us>
2019-10-03 09:53:13 -0700

    added files

--- DrinkingAge.java
+++ DrinkingAge.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class DrinkingAge{
-  
+  public static void main(String [] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How old are you?");
+    int age = scan.nextInt();
+    if (age >= 21)
+    System.out.println("You are old enough to drink");
+    else
+    System.out.println("You aren't old enough to drink");
+  }
 }
