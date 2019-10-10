ad7d246a8a6b88d505d5bba2b8e7c1207d671020
Brandon Drew <drewb@wlhs.wlwv.k12.or.us>
2019-10-10 09:49:18 -0700

    added new files

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Palindrome{
+  public static void main(String [] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter a word that is a palindrome");
+    String word1 = scan.next();
+
+
+  }
+}
