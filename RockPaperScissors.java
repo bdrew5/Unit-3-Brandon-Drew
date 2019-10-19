3f8e51c8175db502a0936eeb926b5dd2cbd8faef
Brandon Drew <drewb@wlhs.wlwv.k12.or.us>
2019-10-19 13:55:53 -0700

    added files

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -3,8 +3,10 @@ import java.util.Random;
 public class RockPaperScissors{
   public static void main(String[]args){
     Scanner scan = new Scanner(System.in);
-    Random rand = new Random(System.in);
+    Random rand = new Random();
 
-    
+    System.out.println("Pick Rock, Paper, or Scissors")
+    String rps = scan.next()
+    int num = rand.nextInt(3) + 1;
   }
 }
