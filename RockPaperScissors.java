87ab0fbdf96102dbe668a0e8e2faafba70d5d5eb
Brandon Drew <drewb@wlhs.wlwv.k12.or.us>
2019-10-19 14:08:03 -0700

    fixed files

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -5,8 +5,17 @@ public class RockPaperScissors{
     Scanner scan = new Scanner(System.in);
     Random rand = new Random();
 
-    System.out.println("Pick Rock, Paper, or Scissors")
-    String rps = scan.next()
+    System.out.println("Pick Rock, Paper, or Scissors");
+    String rps1 = scan.next();
     int num = rand.nextInt(3) + 1;
+    String rps2;
+
+    if (num == 1)
+    rps2 = "Rock";
+    else if (num == 2)
+    rps2 = "Paper";
+    else
+    rps2 = "Scissors";
+
   }
 }
